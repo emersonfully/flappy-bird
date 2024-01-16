e97bc9d6b72b93e95b3ce17e29bad78daa3482f0
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2024-01-16 18:52:12 -0300

    draw the sprite inside the canvas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 const sprites = new Image()
-sprites.src = '../assers/img/sprites.png'
+sprites.src = '../assets/img/sprites.png'
 
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
+
+function loop() {
+    context.drawImage(
+    sprites, // the psrite image
+    0, 0, // Sprite x, Sprite y
+    33, 24, // sprite size
+    10, 50, // draw location inside canvas
+    33, 24 // sprite size inside canvas
+    )
+
+    
+    requestAnimationFrame(loop)
+}
+
+loop()
